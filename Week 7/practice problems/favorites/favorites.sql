b071ed0014dd4cb88e953717d436ed998bf9a42e
Dair66 <alasdairsw.anderson@gmail.com>
2023-11-06 17:49:31 +0000

    Create favorites.sql

---
+++ Week 7/practice problems/favorites/favorites.sql
@@ -0,0 +1,11 @@
+UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title LIKE "Avatar%";
+
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "Brooklyn%";
+
+UPDATE shows SET title = "Game of Thrones" WHERE title LIKE "Game%";
+
+UPDATE shows SET title = "Grey's Anatomy" WHERE title LIKE "%Anatomy%";
+
+UPDATE shows SET title = "The Office" WHERE title LIKE "%Office%";
+
+UPDATE shows SET title = "It's Always Sunny in Philadelphia" WHERE title LIKE "%Philiadelphia%";
